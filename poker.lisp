171a3c8c57a11a30703e671385ea7449d2eb6240
kqr <k@rdw.se>
2018-04-07 10:37:08 +0200

    add deforder macro

--- poker.lisp
+++ poker.lisp
@@ -1,34 +1,67 @@
 (ql:quickload :alexandria)
+(ql:quickload :fset)
 
 (defpackage :org.xkqr.dev.cl.poker
-  (:use :common-lisp :alexandria))
+  (:use :common-lisp :alexandria :fset-user))
 (in-package :org.xkqr.dev.cl.poker)
 
-;; TODO: create a deforder macro which defines an ordering for given symbols
+;; * Define custom, generic orderings
+(defgeneric ord< (a b)
+  (:documentation "Generically check if a is less than b."))
 
-;; * Card
-(defparameter *suits*
-  '(hearts clubs spades diamonds))
+(defmethod ord< ((a null) (b null))
+  nil)
 
-(defparameter *ranks*
+(defmethod ord< ((a cons) (b cons))
+  "Cons pairs are compared lexicographically."
+  (or (ord< (car a) (car b))
+      (and (not (ord< (car b) (car a)))
+           (ord< (cdr a) (cdr b)))))
+
+(defmacro deforder (name symbols)
+  "An order has a NAME and is a set of SYMBOLS associated with a particular
+order. This macro generates convenience functions to deal with it when sorting,
+etc."
+  `(progn
+     (defclass ,name ()
+       ((name
+         :reader name
+         :initarg :value)))
+
+     (defmethod print-object ((value ,name) stream)
+       (print-unreadable-object (value stream :type t)
+         (with-slots (name) value
+           (format stream "~s" name))))
+     
+     (defun ,name (value)
+       (make-instance (quote ,name) :value value))
+
+     (defparameter ,(intern (concatenate 'string "*" (symbol-name name) "S*"))
+       (mapcar #',name ,(car body)))     
+     
+     (defmethod ord< ((a ,name) (b ,name))
+       (< (position (name a) ,(order-symbol name) :key #'name)
+          (position (name b) ,(order-symbol name) :key #'name)))))
+
+;; * Deck of cards
+;; TODO test ordering of orders
+
+(deforder rank
   '(2 3 4 5 6 7 8 9 10 jack queen king ace))
 
-(defun card-suit (card)
-  "Return the suit from a CARD pair (suit . rank)."
-  (car card))
+(deforder suit
+  '(clubs diamonds hearts spades))
+
+(deforder card
+  (map-product #'cons *ranks* *suits*))
 
 (defun card-rank (card)
-  "Return the rank from a CARD pair (suit . rank)."
-  (cdr card))
+  "Return the rank from a CARD."
+  (car (name card)))
 
-(defun rank< (left right)
-  (< (position left *ranks*)
-     (position right *ranks*)))
-
-;; * Deck
-(defparameter *cards*
-  (map-product #'cons *suits* *ranks*)
-  "A list of all cards, each card being a cons pair.")
+(defun card-suit (card)
+  "Return the suit from a CARD."
+  (cdr (name card)))
 
 (defclass deck ()
   ((cards
@@ -44,15 +77,14 @@
   ((hand
     :reader player-hand
     :initform ())
-   (score
-    :reader player-score
+   (wins
+    :reader player-wins
     :initform 0)))
 
 (defmethod print-object ((player player) stream)
   (print-unreadable-object (player stream :type t)
-    (format stream "(~d) ~a"
-            (player-score player)
-            (player-hand player))))
+    (with-slots (hand wins) player
+      (format stream "(~d) ~a" wins hand))))
 
 (defun drop-cards (player &key (keep nil))
   "Drop all cards from PLAYER except those in KEEP."
@@ -68,89 +100,64 @@
 
 (defmethod reject-card ((player newbie) card)
   "Stupid AI rejects any cards with value less than 7."
-  (rank< (card-rank card) 7))
+  (ord< (card-rank card) (rank 7)))
 
 ;; ** Human
 (defclass human (player)
   nil)
 
-;; * Utils
-(defun counts (seq &key (key #'identity))
-  "Each item in SEQ (under KEY) associated with its
-count, returned as a hash table."
-  (loop with counts = (make-hash-table)
-     for elt in seq
-     do (incf (gethash (funcall key elt) counts 0))
-     finally (return counts)))
-
-(defun hash-table-invert (hash-table)
-  "Invert HASH-TABLE, going from a key--value mapping to
-a mapping of key--list-of-values."
-  (loop with inverted = (make-hash-table)
-     for key being the hash-keys of hash-table
-     using (hash-value value)
-     do (push key (gethash value inverted '()))
-     finally (return inverted)))
-
 ;; * Scoring
 (defun hand-counts (hand key)
   "Return how many there are in HAND of each KEY."
-  (hash-table-invert (counts hand :key key)))
+  (let ((counts (fset:convert 'fset:bag (mapcar key hand)))
+        (inverted (fset:empty-map '())))
+    (fset:do-bag-pairs (item mult counts)
+      (push item (fset:lookup inverted mult)))
+    inverted))
 
-(defparameter *hands*
-  '(high-card
-    one-pair
-    two-pairs
-    three-of-a-kind
-    straight
-    flush
-    full-house
-    four-of-a-kind
+(deforder hand-type
+  '(high-card one-pair two-pairs three-of-a-kind
+    straight flush full-house four-of-a-kind
     straight-flush))
 
-(defun score (hand)
+(defparameter ace-low-straight
+  (mapcar #'rank '(ace 2 3 4 5))
+  "Represents the only case in which ace is the lowest card.")
+
+(defun player-score (player)
   "Return the best way to view HAND as a standard poker hand."
-  (let* ((rank-counts (hand-counts hand #'card-rank))
+  (let* ((hand (player-hand player))
+         (rank-counts (hand-counts hand #'card-rank))
          (suit-counts (hand-counts hand #'card-suit))
-         (ranks (sort (mapcar #'card-rank hand) #'rank<))
-         (straight (cond ((equal ranks '(2 3 4 5 ace))
-                          '(ace 2 3 4 5))
+         (ranks (sort (mapcar #'card-rank hand) #'ord<))
+         (straight (cond ((equal ranks ace-low-straight)
+                          ace-low-straight)
                          ((search ranks *ranks*)
                           ranks)))
-         (flush (gethash 5 suit-counts))
-         (quadruple (gethash 4 rank-counts))
-         (triple (gethash 3 rank-counts))
-         (pair (sort (gethash 2 rank-counts) #'rank<))
+         (flush (fset:lookup suit-counts 5))
+         (quadruple (fset:lookup rank-counts 4))
+         (triple (fset:lookup rank-counts 3))
+         (pair (sort (fset:lookup rank-counts 2) #'ord<))
          (high-card (car (last ranks))))
     (cond
-      ((and straight flush) (list 'straight-flush high-card)) 
-      (quadruple (list 'four-of-a-kind (car quadruple))) 
-      ((and triple pair) (list 'full-house (car triple) (car pair))) 
-      (flush (list 'flush high-card)) 
-      (straight (list 'straight high-card)) 
-      (triple (list 'three-of-a-kind (car triple))) 
-      ((= 2 (length pair)) (list 'two-pairs (cadr pair) (car pair))) 
-      (pair (list 'one-pair (car pair)))
-      (t (list 'high-card high-card)))))
-
-(defun list< (left right &key (key #'identity))
-  "Compare two lists LEFT and RIGHT lexicographically under KEY."
-  (and left right
-       (let ((left-val (key (car left)))
-             (right-val (key (car right))))
-         (if (= left-val right-val)
-             (list< (cdr left) (cdr right) :key key)
-             (< left-val right-val)))))
-
-(defun hand< (left right)
-  "Compare two hands LEFT and RIGHT with poker ranking rules."
-  (let* ((left-score (score left))
-         (right-score (score right)) 
-         (left-hand (position (car left-score) *hands*))
-         (right-hand (position (car right-score) *hands*)))
-    (if (= left-hand right-hand)
-        (rank-list< (cdr left-score) (cdr right-score)) 
-        (< left-hand right-hand))))
+      ((and straight flush)
+       (list (hand-type 'straight-flush) high-card)) 
+      (quadruple
+       (list (hand-type 'four-of-a-kind) (car quadruple))) 
+      ((and triple pair)
+       (list (hand-type 'full-house) (car triple) (car pair))) 
+      (flush
+       (list (hand-type 'flush) high-card)) 
+      (straight
+       (list (hand-type 'straight) high-card)) 
+      (triple
+       (list (hand-type 'three-of-a-kind) (car triple))) 
+      ((= 2 (length pair))
+       (list (hand-type 'two-pairs) (cadr pair) (car pair))) 
+      (pair
+       (list (hand-type 'one-pair) (car pair)))
+      (t
+       (list (hand-type 'high-card) high-card)))))
 
 ;; * Game
 (defun deal-cards (player deck)
@@ -174,25 +181,24 @@ a mapping of key--list-of-values."
        for i upfrom 0 do
          (deal-cards player deck)
          (format t "~%Dealt player ~d: ~a"
-                 i (score (player-hand player))))
+                 i (player-score player)))
 
     (loop for player in players
        for i upfrom 0 do
          (replace-cards player deck)
          (format t "~%Player ~d replaced to: ~a"
-                 i (score (player-hand player))))
+                 i (player-score player)))
     
     (loop for player in players
        for best = player then
-         (if (hand< (player-hand best)
-                    (player-hand player))
+         (if (ord< (player-score best)
+                   (player-score player))
              player
              best)
        finally
-         (incf (slot-value best 'score)))
+         (incf (slot-value best 'wins)))
 
     (mapc #'drop-cards players))  
   
   players)
-
 
